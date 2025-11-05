43794adf2857bddece83c1a2d13a51f125993153
Raphael Bely <rbely@enseirb-matmeca.fr>
2025-11-05 10:10:08 +0100

    add select and stat

--- select.sql
+++ select.sql
@@ -31,7 +31,38 @@ SELECT C.* from Clients as Cl
 
 --La liste des clients avec un compte, avec le nombre de commandes qu’ils ont passé, et le montant total.
 
-SELECT count(Co.commande_id), Cl.* from Client as Cl 
+SELECT Cl.client_id, Cl.nom, count(Co.commande_id), SUM(Co.prix_total_remisé) from Client as Cl 
     JOIN Fidelisation as F on F.client_id = Cl.client_id
     JOIN Commandes as Co on Co.client_id = Cl.client_id
-    GROUP BY Cl.commande_id
+    GROUP BY Cl.client_id, Cl.nom;
+
+
+--La liste des restaurants classés par ordre décroissant du coût moyen des plats principaux.
+
+SELECT R.nom, R.restaurant_id, AVG(I.prix_item) as moyenne_prix_plat_principale from Restaurant
+    JOIN Item as I on I.restaurant_id = R.restaurant_id
+    JOIN categorieItems as CI on CI.categorie_id = I.categorie_id
+    WHERE CI.nom_categorie = "Principal"
+    GROUP BY R.nom, R.restaurant_id
+    ORDER BY DESC moyenne_prix_plat_principale;
+
+
+
+--La liste des restaurant de Bordeaux, avec le nombre de commandes passées durant les 30 derniers jours.
+
+SELECT R.restaurant_id, R.nom, COUNT(Co.commande_id) AS nb_commandes 
+    FROM  restaurant AS R
+    JOIN Commande AS Co ON R.restaurant_id = Co.restaurant_id
+    WHERE UPPER(R.adresse) LIKE UPPER('%Bordeaux%')
+        AND Co.date_commande >= (NOW() - INTERVAL '30 days')
+    GROUP BY R.restaurant_id, R.nom;
+
+
+--Chaque restaurant doit pouvoir consulter les statistiques de commandes de chaque plat par mois, pour l’année écoulée.
+
+SELECT R.restaurant_id, R.nom, MONTHNAME(Co.date_commande) ,count(A.items_id) as Nb_commande, I.items_id, I.nom from Restaurant as R 
+    NATURAL JOIN Item as I
+    JOIN appartenir as A on A.items_id = I.items_id
+    JOIN Commande as Co on Co.commande_id = A.commande_id
+    WHERE Co.date_commande >= (NOW() - INTERVAL '1 year')
+    GROUP BY R.restaurant_id, R.nom, MONTHNAME(Co.date_commande),I.items_id, I.nom
