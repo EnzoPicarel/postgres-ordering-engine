0205489acd5f2f6046d7b1ba0a3d8d3686bffbb1
Enzo Picarel <enzo.picarel@bordeaux-inp.fr>
2025-12-04 15:35:17 +0100

    feat: complements php

---
+++ src/models/Complements.php
@@ -0,0 +1,40 @@
+<?php
+
+class Complements
+{
+    private $conn;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function getComplements($item_id)
+    {
+        $query = Query::loadQuery('sql_requests/getComplements.sql');
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $item_id);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function addComplement($item_id1, $item_id2)
+    {
+        $query = Query::loadQuery('sql_requests/addComplement.sql');
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $item_id1);
+        $stmt->bindParam(2, $item_id2);
+        return $stmt->execute();
+    }
+
+    public function deleteComplement($item_id1, $item_id2)
+    {
+        $query = Query::loadQuery('sql_requests/deleteComplement.sql');
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $item_id1);
+        $stmt->bindParam(2, $item_id2);
+        return $stmt->execute();
+    }
+}
+
+?>
